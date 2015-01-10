45149ddf65f88bdb8dd300ce730c0677e2ad9796
Riccardo De Contardi <erredeco@gmail.com>
2015-01-10 18:58:33 +0100

    version 0.0.2

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,26 +3,16 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('./package.json'),
+    vendor: grunt.file.readJSON('.bowerrc').directory,
+
+    foundation: {
+          js: ['./<%= vendor %>/bower-foundation/js/foundation/foundation.js', './<%= vendor %>/bower-foundation/js/foundation/foundation.*.js']
+        },
 
 
-
-
-
-    clean: ['./dist/'],
-
-    connect: {
-      dist: {
-        options: {
-          port: 8080,
-          base: './dist/',
-          hostname: 'localhost'
-        }
-      }
-    },
 
     assemble: {
       options: {
-  
         flatten: false,
         assets: './dist/assets',
         partials: ['./source/includes/partials/**/*.{html,hbs}'],
@@ -37,7 +27,59 @@ module.exports = function (grunt) {
       }
     },
 
+    //Get components from bower
+    bowercopy: {
+      options: {            
+        srcPrefix: '<%= vendor %>',
+        clean: false
+      },
 
+      // Javascript libraries
+      libs: {
+        options: {
+          destPrefix: './dist/assets/js/vendor'
+        },
+        files: {
+          'jquery.js': 'jquery/dist/jquery.js',
+          'modernizr.js': 'modernizr/modernizr.js',
+          'foundation': 'bower-foundation/js/foundation'
+          },
+      },
+    },
+
+    concat: {
+      dist: {
+        files: {
+          'dist/assets/js/vendor/foundation.js':'<%= foundation.js %>'
+        }
+      }
+    },
+
+    uglify: {
+      options: {
+        preserveComments: 'some'
+      },
+      dist: {
+        files: {
+          './dist/assets/js/foundation.min.js': ['<%= foundation.js %>'],
+          './dist/assets/js/modernizr.min.js': ['<%= vendor %>/modernizr/modernizr.js'],
+          './dist/assets/js/jquery.min.js': ['<%= vendor %>/jquery/dist/jquery.js']
+        }
+      },
+
+    },
+
+    clean: ['./dist/'],
+
+    connect: {
+      dist: {
+        options: {
+          port: 8080,
+          base: './dist/',
+          hostname: 'localhost'
+        }
+      }
+    },
 
     watch: {
       grunt: {
@@ -46,6 +88,14 @@ module.exports = function (grunt) {
         },
         files: ['Gruntfile.js']
       },
+
+      js: {
+        files: ['<%= vendor %>/**/*.js', 'dist/assets/js/**/*.js'],
+        tasks: ['bowercopy','concat','uglify'],
+        options: {livereload:true}
+      },
+
+
       assemble_all: {
         files: ['./source/{includes,layouts}/**/*.html'],
         tasks: ['assemble'],
@@ -56,28 +106,26 @@ module.exports = function (grunt) {
         tasks: ['newer:assemble'],
         options: {livereload:true}
       }
-
-
     }
+
 
   });
 
-  /* load every plugin in package.json */
+  /* load plugin in package.json */
 
   grunt.loadNpmTasks('assemble');
   grunt.loadNpmTasks('grunt-contrib-clean'); 
   grunt.loadNpmTasks('grunt-contrib-connect'); 
   grunt.loadNpmTasks( 'grunt-contrib-watch' );  
   grunt.loadNpmTasks('grunt-newer');
-
+  grunt.loadNpmTasks('grunt-bowercopy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   /* grunt tasks */
   //see the foundation example on how to add other tasks here!!
-  grunt.registerTask('build:assets', ['clean']); 
+  grunt.registerTask('build:assets', ['clean','bowercopy','concat','uglify']); 
   grunt.registerTask('build', ['build:assets', 'assemble']);
   grunt.registerTask('default', ['build', 'watch']);
   grunt.registerTask('server', ['connect:dist:keepalive']);
-
-  // Serve presentation locally; must be the LAST!
-  //grunt.registerTask( 'default', ['assemble', 'connect'] );
 };
